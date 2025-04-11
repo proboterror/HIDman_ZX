78d3975dc4fc8f1e15b214186c863172b5cd4449
probotector <probotector@yandex.ru>
2025-04-11 18:30:07 +0300

    ZX BUS adapter EPM3128ATC100 Verilog implementation and testbench.
    107/128 macrocells.
    Simulation passed testbench tests.
    
    Implemented:
    - Kempston Mouse registers
    - Kempston Joystick 8-bit register
    - 40-key keyboard matrix on port #FE with serial interface compatible with CH446Q
    - Special keys for PAUSE, MAGIC, RESET (Z80 BSRQ/, NMI/, RESET/ control)
    
    - Reset keyboard registers state on incoming RESET
    - Option to disable Kempston Joystick interface (JOY_ENABLE pin)
    
    Disabled:
    - Mouse IORQGE
    - Mouse set registers initial values on reset
    - Joystick set registers initial values on reset
    
    Not implemented:
    - RESET hotkey register resets on self initiated reset
    - IORQGE tests
    
    ToDo:
    - Investigate TAPE IN port #FE bit 6 blocking

---
+++ source/Altera/hidman_zx_bus.v
@@ -0,0 +1,351 @@
+/*
+ ZX BUS Kempston Mouse controller
+
+ Kempston mouse ports:
+ #FADF 1111 1010 1101 1111 BUTTONS
+ #FBDF 1111 1011 1101 1111 MX
+ #FFDF 1111 1111 1101 1111 MY
+
+ Port #FADF: buttons and mouse wheel.
+  D0: left button (0 = pressed)
+  D1: right button (0 = pressed)
+  D2: middle button (0 = pressed)
+  D3: reserved (default 1)
+  D4-D7: mouse wheel axis
+
+   Note on mouse wheel:
+   https://velesoft.speccy.cz/kmouse/km-doc/kempston_mouse_turbo_interface/km-t_2011/k-mouse2011-doc.pdf:
+   If wheel is off then D4-D7 on button port return 1111.
+   https://www.benophetinternet.nl/hobby/kmt/index.htm
+   Bit 4-7 (D4-D7) return the position of the mouse wheel = %1111 is default
+
+ Port #FBDF: X axis (increases from left to right)
+ Port #FFDF: Y axis (increases from down to up)
+*/
+module kempston_mouse
+(
+	input wire MX, MY, MKEY,
+	input wire[7:0] DI,
+
+	input wire [15:0] A,
+	input wire M1, // 1
+	input wire RD, // active low
+	input wire IORQ, // active low
+
+	input wire rst_in,
+
+	output wire IORQGE,
+	output wire enable,
+
+	output wire[7:0] D
+);
+
+	reg[7:0] register_x, register_y, register_key;
+/*
+	always @(negedge rst_in or posedge MX or posedge MY or posedge MKEY)
+	begin
+		if(rst_in == 0) // ZX BUS Z80 reset
+		begin
+			register_x = 8'h80;
+			register_y = 8'h60;
+			register_key = 8'hFF;
+		end
+		else if(MX == 1)
+			register_x = DI;
+		else if(MY == 1)
+			register_y = DI;
+		else if(MKEY == 1)
+			register_key = DI;
+	end
+*/
+	// Data written to registers on rising_edge.
+	always @(posedge MX) register_x = DI;
+	always @(posedge MY) register_y = DI;
+	always @(posedge MKEY) register_key = DI;
+
+	// Address lower bits: #DF 11011111 1x0xxx11
+	//wire address_partial_match = ~((A[7:0] == 8'hDF) & M1); // more macrocells
+	wire address_partial_match = ~(A[0] & A[1] & ~A[5] & A[7] & A[15] & M1); // A15 or DOS/ ?
+	assign IORQGE = address_partial_match; // CPLD IORQGE pin connected to 3-state buffer OE/ pin.
+	assign enable = ~(address_partial_match | RD | IORQ);
+
+	wire MKEY_SEL = enable & ~A[8] & ~A[10]; // #FA 11111010 xxxxx0x0
+	wire MX_SEL = enable & A[8] & ~A[10];    // #FB 11111011 xxxxx0x1
+	wire MY_SEL = enable & A[8] & A[10];     // #FF 11111111 xxxxx1x1
+
+	assign D = 
+		MX_SEL ? register_x :
+		MY_SEL ? register_y :
+		MKEY_SEL ? {register_key[7:4], 1'b1, register_key[2:0]} : 8'b0/*unused*/; // 3rd bit == 1 and can be omitted.
+endmodule
+
+module kempston_joy
+(
+	input wire JOY,
+	input wire[7:0] DI,
+
+	input wire [15:0] A,
+	input wire M1, // 1
+	input wire RD, // active low
+	input wire IORQ, // active low
+
+	input wire rst_in,
+
+	input wire JOY_ENABLE,
+
+	output wire IORQGE,
+	output wire enable,
+
+	output wire[7:0] D
+);
+
+	reg[7:0] register_joy;
+/*
+	always @(negedge rst_in or posedge JOY)
+	begin
+		if(rst_in == 0) // ZX BUS Z80 reset
+			register_joy = 8'h0;
+		else 
+			register_joy = DI;
+	end
+*/
+	always @(posedge JOY) register_joy = DI;
+
+	// Port #1F/31 xxxxxxxx00011111 
+	//wire address_match = ~((A[7:0] == 8'h1F) & M1 & ~JOY_ENABLE);
+	wire address_match = (A[7] | A[6] | A[5] | ~M1 | JOY_ENABLE);
+	assign IORQGE = address_match;
+	assign enable = ~(address_match | RD | IORQ);
+
+	assign D = register_joy;
+endmodule
+
+/*
+ZX Spectrum 40-key keyboard matrix:
+         ┌─────────────────────────────────────────────────────┐
+         │     ┌─────────────────────────────────────────┐     │
+         │     │     ┌─────────────────────────────┐     │     │
+         │     │     │     ┌─────────────────┐     │     │     │
+         │     │     │     │     ┌─────┐     │     │     │     │
+       ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐
+ A11/ ─┤ 1 ├─┤ 2 ├─┤ 3 ├─┤ 4 ├─┤ 5 │ │ 6 ├─┤ 7 ├─┤ 8 ├─┤ 9 ├─┤ 0 ├─ A12/
+       └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘
+       ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐
+ A10/ ─┤ Q ├─┤ W ├─┤ E ├─┤ R ├─┤ T │ │ Y ├─┤ U ├─┤ I ├─┤ O ├─┤ P ├─ A13/
+       └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘
+       ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐
+  A9/ ─┤ A ├─┤ S ├─┤ D ├─┤ F ├─┤ G │ │ H ├─┤ J ├─┤ K ├─┤ L ├─┤Ent├─ A14/
+       └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘
+       ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐
+  A8/ ─┤ CS├─┤ Z ├─┤ X ├─┤ C ├─┤ V │ │ B ├─┤ N ├─┤ M ├─┤ SS├─┤ SP├─ A15/
+       └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘
+         D0    D1    D2    D3    D4    D4    D3    D2    D1    D0
+*/
+/*
+ Emulates 8x16 Analog Switch Array Chip CH446Q in serial mode.
+ Address range limited to AY: 0..4, AX: 0..7.
+ Special keys:
+ ZX_KEY_MAGIC: Y:5 X:8 ?????
+ ZX_KEY_RESET: Y:6 X:8
+ ZX_KEY_PAUSE: Y:7 X:8
+*/
+/*
+ 8x16 Analog Switch Array Chip CH446Q
+ Reference: CH446 Datasheet http://wch.cn
+
+ CH446Q is an 8x16 matrix analog switch chip. CH446Q contains 128 analog switches, which are distributed
+ at each cross point of 8x16 signal channel matrices.
+
+ - CH446Q supports 7-bit parallel address input and is compatible with existing similar products.
+ - Support serial address shift input to save pins.
+
+ Pins:
+ - DAT Input Serial data input and switch data input in serial address mode; On at high level, off at low level
+ - STB Input Strobe pulse input, active at high level
+ - CK Input Serial clock input in serial address mode, active on rising edge;
+
+ - X0~X15 Analog signal Input/Output Port X of 8x16 matrix analog switch
+ - Y0~Y7 Analog signal Input/Output Port Y of 8x16 matrix analog switch
+
+ The following table is the decoding truth table of the 7-bit address ADDR for CH446Q chip and the address table of 128 analog switches.
+
+ Intersection
+ Point          ADDR6 ADDR5 ADDR4 ADDR3 ADDR2 ADDR1 ADDR0 Address
+ Port Y -        AY2   AY1   AY0   AX3   AX2   AX1   AX0    No.
+ Port X
+ Y0 - X0          0     0     0     0     0     0     0     00H
+ Y0 - X1          0     0     0     0     0     0     1     01H
+ Y0 - X2          0     0     0     0     0     1     0     02H
+ В·В·В·В·В·В·
+ Y7 - X14         1     1     1     1     1     1     0     7EH
+ Y7 - X15         1     1     1     1     1     1     1     7FH
+
+ The figure below is an example of a serial address input that controls the analog switch with 24H address
+ (between Y2 = 010b and X4 = 0100b), first on and then off.
+
+       ─┐   ┌───┐       ┌───┐       ┌────────┐        ┌───┐
+ DAT     AY2│AY1│AY0|AX3│AX2│AX1|AX0│ ON/OFF │ ON/OFF │AY2│AY1
+       ─┴───┘   └───────┘   └───────┘        └────────┘   └────
+           0   1   0   0   1   0   0   ON       OFF      1   0
+
+ CS/SK ──┐ ┌─┐ ┌─┐ ┌─┐ ┌─┐ ┌─┐ ┌─┐ ┌─┬─────────────────┐ ┌─┐ ┌─
+       ──┴─┘ └─┘ └─┘ └─┘ └─┘ └─┘ └─┘ └─────────────────┴─┘ └─┘ 
+                                       ┌─┐      ┌─┐
+ STB   ────────────────────────────────┘ └──────┘ └────────────
+
+ Serial Address Input
+  Control steps in serial address input mode: provide 7-bit address through DAT pin and move into CH446 by
+  using 7 rising edges of CS/CK pin, provide data through DAT pin, and provide a high level pulse to STB pin.
+*/
+module keyboard
+(
+	input wire DAT, // Serial data (7 bit), write on rising edge
+	input wire SK, // Serial clock
+	input wire STB, // Serial transfer done / set switch state strobe
+
+	input wire [15:0] A,
+	input wire M1,
+	input wire RD,
+	input wire IORQ,
+
+	input wire rst_in,
+
+	output wire [7:0] D,
+	output wire IORQGE,
+	output wire enable,
+
+	output wire PAUSE, MAGIC, RESET // Active Low (init?)
+);
+	reg[6:0] serial_data;
+	reg[4:0] keys [8/*sv*//*7:0*/] /*= 1*/; // 1 - key released, 0 - key pressed
+	reg REG_PAUSE, REG_MAGIC, REG_RESET;
+
+	// ToDo: set keys[] and special keys to 1 on bus /RST (do not set on self-reset on hotkey)
+/*
+    if (rst_i == 1'd0) 
+        k <= {42{1'b1}};                   // initialization on power on: set all bits
+    else
+        if (spi[0] == 1'd1)
+        begin
+            k[key_bit_num] <= key_state;   // set or clear state-bit
+            if (key_bit_num == 41)
+                k[40:0] = 41'h1FFFFFFFFFF; // cpu reset and initialization: set all bits except 'reset'
+        end
+*/
+
+	always @(posedge SK) // serial_data <= { serial_data[5:0], DAT }
+	begin
+		serial_data = serial_data << 1;
+		serial_data[0] = DAT;
+	end
+
+	wire[3:0] AX = serial_data[3:0]; // X range 0..7
+	wire[2:0] AY = serial_data[6:4]; // Y range 0..5
+	wire key_state = DAT;
+
+	always @(negedge rst_in or posedge STB)
+	begin
+		if(rst_in == 0) // ZX BUS Z80 reset
+		begin
+			keys[0] = 5'b11111;
+			keys[1] = 5'b11111;
+			keys[2] = 5'b11111;
+			keys[3] = 5'b11111;
+			keys[4] = 5'b11111;
+			keys[5] = 5'b11111;
+			keys[6] = 5'b11111;
+			keys[7] = 5'b11111;
+			// BSRQ, NMI, RST set to hi-Z on bus RESET
+			REG_PAUSE = 1;
+			REG_MAGIC = 1;
+			REG_RESET = 1;
+		end
+		else begin // posedge STB
+			if(AX[3] == 1'b1) // X = 8 for special keys.
+			begin
+				if(AY == 3'b101 /*5*/) REG_MAGIC = key_state;
+				// Note: rst_in will set REG_RESET to 1... https://en.wikipedia.org/wiki/Ouroboros
+				if(AY == 3'b110 /*6*/) REG_RESET = key_state;
+				if(AY == 3'b111 /*7*/) REG_PAUSE = key_state;
+			end
+			else
+				keys[AX][AY] = key_state;
+		end
+	end
+
+	// Output signal active low; 0 - hotkey pressed;
+	assign PAUSE = ~REG_PAUSE ? 1'b0 : 1'bZ;
+	assign MAGIC = ~REG_MAGIC ? 1'b0 : 1'bZ;
+	assign RESET = ~REG_RESET ? 1'b0 : 1'bZ;
+
+	wire [4:0] half_rows_state =
+	(
+		((~A[8])?  keys[0] : 5'b11111) &
+		((~A[9])?  keys[1] : 5'b11111) &
+		((~A[10])? keys[2] : 5'b11111) &
+		((~A[11])? keys[3] : 5'b11111) &
+		((~A[12])? keys[4] : 5'b11111) &
+		((~A[13])? keys[5] : 5'b11111) &
+		((~A[14])? keys[6] : 5'b11111) &
+		((~A[15])? keys[7] : 5'b11111)
+	);
+
+	// Port #FE/254 xxxxxxxx11111110
+	// What about TAPE IN port #FE bit 6?
+	wire address_partial_match = ~((A[0] == 1'b0) & M1);
+	//wire address_partial_match = ~((A[7:0] == 8'hFE) & M1);
+	assign IORQGE = address_partial_match;
+	assign enable = ~(address_partial_match | RD | IORQ);
+
+	assign D[7:0] = {3'bZZZ, half_rows_state};
+
+endmodule
+
+module hidman_zx_bus
+(
+	// ToDo: Add ZX BUS pin CLK
+
+	input wire MX, MY, MKEY,
+	input wire JOY,
+	input wire[7:0] DI,
+
+	input wire JOY_ENABLE, // active low
+
+	input wire [15:0] A,
+	input wire M1, // 1
+	input wire RD, // active low
+	input wire IORQ, // active low
+	input wire RST_IN,
+
+	output wire IORQGE,
+
+	output wire[7:0] D,
+
+	input wire DAT,
+	input wire SK,
+	input wire STB,
+
+	output wire BSRQ, NMI, RST_OUT // Active Low.
+);
+	wire iorqge_mouse, iorqge_keyboard, iorqge_joy;
+	wire en_m, en_k, en_j;
+	wire[7:0] d_m, d_k, d_j;
+
+	kempston_mouse mouse(MX, MY, MKEY, DI, A, M1, RD, IORQ, RST_IN, iorqge_mouse, en_m, d_m);
+	keyboard key(DAT, SK, STB, A, M1, RD, IORQ, RST_IN, d_k, iorqge_keyboard, en_k, BSRQ, NMI, RST_OUT);
+	kempston_joy pad(JOY, DI, A, M1, RD, IORQ, RST_IN, JOY_ENABLE, iorqge_joy, en_j, d_j);
+
+	// IORQGE = 0 when address lower bits and M1 == 1 (address partial match) else = 1.
+	// Connected to 74LVC1G125 3-state buffer OE/ pin, TTL 5V output.
+	assign IORQGE = 
+		//iorqge_mouse &
+		iorqge_keyboard &
+		iorqge_joy;
+
+	assign D =
+		en_m ? d_m :
+		en_k ? d_k :
+		en_j ? d_j :
+		8'bZZZZZZZZ;
+
+endmodule
---
+++ source/Altera/testbench.v
@@ -0,0 +1,312 @@
+`timescale 1ns/1ps
+module testbench();
+
+reg MX, MY, MKEY, JOY; 
+reg M1, RD, IORQ, RST_IN;
+reg[7:0] DI;
+reg[15:0] A;
+
+reg DAT, SK, STB;
+
+wire[7:0] D;
+wire IORQGE;
+
+wire BSRQ, NMI, RST_OUT; 
+
+hidman_zx_bus hidman_zx_bus0(
+	.MX(MX),
+	.MY(MY),
+	.MKEY(MKEY),
+	.JOY(JOY),
+	.DI(DI),
+
+	.JOY_ENABLE(1'b0), // active low
+
+	.A(A),
+	.M1(M1), // 1
+	.RD(RD), // active low
+	.IORQ(IORQ), // active low
+	.RST_IN(RST_IN),
+
+	.IORQGE(IORQGE),
+	.D(D),
+
+	.BSRQ(BSRQ),
+	.NMI(NMI),
+	.RST_OUT(RST_OUT),
+
+	.DAT(DAT),
+	.SK(SK),
+	.STB(STB)
+);
+
+// https://verificationacademy.com/forums/t/what-is-meant-by-reference-argument-must-be-automatic/29753
+task automatic ControllerWrite(ref logic strobe, input reg [7:0] data);
+	begin
+		DI = data;
+		#2
+		strobe = 1;
+		#2
+		strobe = 0;
+	end
+endtask
+
+task PortRead(input reg[15:0] addr);
+	begin
+		M1 = 1;
+		A = addr;
+		#2
+		RD = 0;
+		IORQ = 0;
+		#2;
+	end
+endtask
+
+task keyboard_write_bit(input logic data);
+	begin
+		SK = 0;
+		#2
+		DAT = data;
+		#2
+		SK = 1;
+		#2;
+	end
+endtask
+
+task keyboard_commit_state(input logic state);
+	begin
+		DAT = state;
+		#2
+		STB = 1;
+		#2
+		STB = 0;
+	end
+endtask
+
+/*
+enum CH446Q_AY
+{
+	D0 = 0 << 4,
+	D1 = 1 << 4,
+	D2 = 2 << 4,
+	D3 = 3 << 4,
+	D4 = 4 << 4
+};
+
+enum CH446Q_AX
+{
+	A8  = 0,
+	A9  = 1,
+	A10 = 2,
+	A11 = 3,
+	A12 = 4,
+	A13 = 5,
+	A14 = 6,
+	A15 = 7,
+};
+*/
+
+task keyboard_set_switch(input logic[2:0] Y, input logic[3:0] X, input logic state);
+	begin
+		keyboard_write_bit(Y[2]);
+		keyboard_write_bit(Y[1]);
+		keyboard_write_bit(Y[0]);
+
+		keyboard_write_bit(X[3]);
+		keyboard_write_bit(X[2]);
+		keyboard_write_bit(X[1]);
+		keyboard_write_bit(X[0]);
+
+		keyboard_commit_state(state);
+	end
+endtask
+
+task reset();
+	begin
+		RST_IN = 0;
+		#2
+		RST_IN = 1;
+		#2;
+	end;
+endtask;
+
+initial begin
+	MX = 0; 
+	MY = 0;
+	MKEY = 0;
+	JOY = 0;
+	DI = 0;
+
+	DAT = 0;
+	SK = 0;
+	STB = 0;
+
+	RD = 1;
+	IORQ = 1;
+	M1 = 1;
+
+	A = 16'hFFFF;
+
+	reset();
+	// ToDo: IORQGE tests: address + M1.
+/*
+	Check mouse registers state ater reset
+	#FADF 1111 1010 1101 1111 BUTTONS
+	#FBDF 1111 1011 1101 1111 MX
+	#FFDF 1111 1111 1101 1111 MY
+*/
+/*
+	PortRead(16'hFBDF);
+	assert (D === 8'h80) else $error("MX reset failed.");
+	#10
+	PortRead(16'hFFDF);
+	assert (D === 8'h60) else $error("MY resetfailed.");
+	#10
+	PortRead(16'hFADF);
+	assert (D === 8'hAA) else $error("MKEY reset failed.");
+	#10
+*/
+	// Set mouse controller registers
+	ControllerWrite(MX, 8'hCC);
+	#10
+	ControllerWrite(MY, 8'h55);
+	#10
+	ControllerWrite(MKEY, 8'hAA);
+	#10
+	// Check Kempston Mouse controller ports.
+	PortRead(16'hFBDF);
+	assert (D === 8'hCC) else $error("MX set failed.");
+	#10
+	PortRead(16'hFFDF);
+	assert (D === 8'h55) else $error("MY set failed.");
+	#10;
+	PortRead(16'hFADF);
+	assert (D === 8'hAA) else $error("MKEY set failed."); // 3rd bit always == 1 (reserved/unused)
+	#10
+/*
+	// Check Kempston Joystick state after reset.
+	PortRead(16'h001F);
+	assert (D === 8'h0) else $error("JOY reset failed.");
+	#10;
+*/
+	// Set and check Kempston Joystick 8-bit port #1F
+	A = 16'hFFFF;
+	ControllerWrite(JOY, 8'hAA);
+	#10
+	PortRead(16'h001F);
+	assert (D === 8'hAA) else $error("JOY set failed.");
+	#10;
+/*
+	Keyboard port xxFE
+	Space...B	7FFE 01111111 11111110
+	Enter...H	BFFE 10111111 11111110
+	P...V		DFFE 11011111 11111110
+	0...6		EFFE 11101111 11111110
+	1...5		F7FE 11110111 11111110
+	Q...T		FBFE 11111011 11111110
+	A...G		FDFE 11111101 11111110
+	CS...V		FEFE 11111110 11111110
+         ┌─────────────────────────────────────────────────────┐
+         │     ┌─────────────────────────────────────────┐     │
+         │     │     ┌─────────────────────────────┐     │     │
+         │     │     │     ┌─────────────────┐     │     │     │
+         │     │     │     │     ┌─────┐     │     │     │     │
+       ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐
+ A11/ ─┤ 1 ├─┤ 2 ├─┤ 3 ├─┤ 4 ├─┤ 5 │ │ 6 ├─┤ 7 ├─┤ 8 ├─┤ 9 ├─┤ 0 ├─ A12/
+       └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘
+       ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐
+ A10/ ─┤ Q ├─┤ W ├─┤ E ├─┤ R ├─┤ T │ │ Y ├─┤ U ├─┤ I ├─┤ O ├─┤ P ├─ A13/
+       └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘
+       ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐
+  A9/ ─┤ A ├─┤ S ├─┤ D ├─┤ F ├─┤ G │ │ H ├─┤ J ├─┤ K ├─┤ L ├─┤Ent├─ A14/
+       └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘
+       ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐ ┌─┴─┐
+  A8/ ─┤ CS├─┤ Z ├─┤ X ├─┤ C ├─┤ V │ │ B ├─┤ N ├─┤ M ├─┤ SS├─┤ SP├─ A15/
+       └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘ └─┬─┘
+         D0    D1    D2    D3    D4    D4    D3    D2    D1    D0
+*/
+	// Check keyboard rows state after reset.
+	A = {8'b11111110, 8'hFE}; // [CS..V]
+	#20;
+	assert (D[4:0] === 5'b11111) else $error("Keyboard row [CS..V] reset failed.");
+
+	A = {8'b01111111, 8'hFE}; // [B..SP]
+	#20;
+	assert (D[4:0] === 5'b11111) else $error("Keyboard row [B..SP] reset failed.");
+
+	// AY, AX, state
+	keyboard_set_switch(3'h0, 3'h0, 0); // A8 [CS..V]
+	keyboard_set_switch(3'h1, 3'h1, 0); // A9 [A..G]
+	keyboard_set_switch(3'h2, 3'h2, 0); // A10 [Q..T]
+
+	keyboard_set_switch(3'h0, 3'h3, 1); // A11 [1..5]
+	keyboard_set_switch(3'h1, 3'h3, 0);
+	keyboard_set_switch(3'h2, 3'h3, 1);
+	keyboard_set_switch(3'h3, 3'h3, 0);
+	keyboard_set_switch(3'h4, 3'h3, 1);
+
+	keyboard_set_switch(3'h4, 3'h4, 0); // A12 [6..0]
+	keyboard_set_switch(3'h3, 3'h5, 0); // A13 [Y..P]
+	keyboard_set_switch(3'h2, 3'h6, 0); // A14 [H..Ent]
+	keyboard_set_switch(3'h1, 3'h7, 0); // A15 [B..SP]
+
+	// Read keyboard single rows
+	A = {8'b11111110, 8'hFE}; // [CS..V]
+	#20;
+	assert (D[4:0] === 5'b11110) else $error("Keyboard row [CS..V] failed.");
+
+	A = {8'b11111101, 8'hFE}; // [A..G]
+	#20;
+	assert (D[4:0] === 5'b11101) else $error("Keyboard row [A..G] failed.");
+
+	A = {8'b11111011, 8'hFE}; // [Q..T]
+	#20;
+	assert (D[4:0] === 5'b11011) else $error("Keyboard row [Q..T] failed.");
+
+	A = {8'b11110111, 8'hFE}; // [1..5]
+	#20;
+	assert (D[4:0] === 5'b10101) else $error("Keyboard row [1..5] failed.");
+
+	A = {8'b11101111, 8'hFE}; // [6..0]
+	#20;
+	assert (D[4:0] === 5'b01111) else $error("Keyboard row [6..0] failed.");
+
+	A = {8'b11011111, 8'hFE}; // [Y..P]
+	#20;
+	assert (D[4:0] === 5'b10111) else $error("Keyboard row [Y..P] failed.");
+
+	A = {8'b10111111, 8'hFE}; // [H..Ent]
+	#20;
+	assert (D[4:0] === 5'b11011) else $error("Keyboard row [H..Ent] failed.");
+
+	A = {8'b01111111, 8'hFE}; // [B..SP]
+	#20;
+	assert (D[4:0] === 5'b11101) else $error("Keyboard row [B..SP] failed.");
+
+	// Read keyboard multiple rows
+	A = {8'b00111111, 8'hFE}; // [H..Ent], [B..SP]
+	#20;
+	assert (D[4:0] === 5'b11001) else $error("Keyboard multiple rows failed.");
+/*
+	Special keys:
+	ZX_KEY_MAGIC: Y:5 X:8
+	ZX_KEY_RESET: Y:6 X:8
+	ZX_KEY_PAUSE: Y:7 X:8
+*/
+	assert (NMI === 1'hZ) else $error("NMI active.");
+	assert (RST_OUT === 1'hZ) else $error("RST_OUT active.");
+	assert (BSRQ === 1'hZ) else $error("BSRQ active.");
+
+	// AY, AX, state
+	keyboard_set_switch(3'h5, 4'h8, 0);
+	#10;
+	assert (NMI === 0) else $error("NMI set active failed.");
+	keyboard_set_switch(3'h6, 4'h8, 0);
+	#10;
+	assert (RST_OUT === 0) else $error("RST_OUT set active failed.");
+	keyboard_set_switch(3'h7, 4'h8, 0);
+	#10;
+	assert (BSRQ === 0) else $error("BSRQ set active failed.");
+end
+
+endmodule
