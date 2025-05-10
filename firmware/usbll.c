79be7b3e9a9d1783cb068ccf4672d3fc82d679bc
probotector <probotector@yandex.ru>
2025-05-10 17:27:15 +0300

    Return back original WinChipHead comments (simplified chinese). Converted to UTF-8. Source: https://github.com/zhuhuijia0001/ch559-usb-host

--- firmware/usbll.c
+++ firmware/usbll.c
@@ -145,8 +145,8 @@ void SetUsbSpeed(UINT8 FullSpeed) // set current speed
 {
 	if (FullSpeed)
 	{
-		USB_CTRL &= ~bUC_LOW_SPEED;	 // ȫ��
-		UH_SETUP &= ~bUH_PRE_PID_EN; // ��ֹPRE PID
+		USB_CTRL &= ~bUC_LOW_SPEED;	 // 全速
+		UH_SETUP &= ~bUH_PRE_PID_EN; // 禁止PRE PID
 	}
 	else
 	{
@@ -157,23 +157,23 @@ void SetUsbSpeed(UINT8 FullSpeed) // set current speed
 void ResetRootHubPort(UINT8 RootHubIndex)
 {
 	SetHostUsbAddr(0x00);
-	SetUsbSpeed(1); // Ĭ��Ϊȫ��
+	SetUsbSpeed(1); // 默认为全速
 
 	if (RootHubIndex == 0)
 	{
-		UHUB0_CTRL = UHUB0_CTRL & ~bUH_LOW_SPEED | bUH_BUS_RESET; // Ĭ��Ϊȫ��,��ʼ��λ
-		mDelaymS(15);											  // ��λʱ��10mS��20mS
-		UHUB0_CTRL = UHUB0_CTRL & ~bUH_BUS_RESET;				  // ������λ
+		UHUB0_CTRL = UHUB0_CTRL & ~bUH_LOW_SPEED | bUH_BUS_RESET; // 默认为全速,开始复位
+		mDelaymS(15);											  // 复位时间10mS到20mS
+		UHUB0_CTRL = UHUB0_CTRL & ~bUH_BUS_RESET;				  // 结束复位
 	}
 	else if (RootHubIndex == 1)
 	{
-		UHUB1_CTRL = UHUB1_CTRL & ~bUH_LOW_SPEED | bUH_BUS_RESET; // Ĭ��Ϊȫ��,��ʼ��λ
-		mDelaymS(15);											  // ��λʱ��10mS��20mS
-		UHUB1_CTRL = UHUB1_CTRL & ~bUH_BUS_RESET;				  // ������λ
+		UHUB1_CTRL = UHUB1_CTRL & ~bUH_LOW_SPEED | bUH_BUS_RESET; // 默认为全速,开始复位
+		mDelaymS(15);											  // 复位时间10mS到20mS
+		UHUB1_CTRL = UHUB1_CTRL & ~bUH_BUS_RESET;				  // 结束复位
 	}
 
 	mDelayuS(250);
-	UIF_DETECT = 0; // ���жϱ�־
+	UIF_DETECT = 0; // 清中断标志
 }
 
 void SelectHubPort(UINT8 RootHubIndex, UINT8 HubPortIndex)
@@ -202,7 +202,7 @@ void InitUsbHost(void)
 	UINT8 i;
 	IE_USB = 0;
 
-	USB_CTRL = bUC_HOST_MODE; // ���趨ģʽ
+	USB_CTRL = bUC_HOST_MODE; // 先设定模式
 	USB_DEV_AD = 0x00;
 	UH_EP_MOD = bUH_EP_TX_EN | bUH_EP_RX_EN;
 	UH_RX_DMA = 0x0000;
@@ -212,13 +212,13 @@ void InitUsbHost(void)
 
 	UHUB1_CTRL &= ~bUH1_DISABLE; //enable root hub1
 
-	USB_CTRL = bUC_HOST_MODE | bUC_INT_BUSY | bUC_DMA_EN; // ����USB������DMA,���жϱ�־δ���ǰ�Զ���ͣ
+	USB_CTRL = bUC_HOST_MODE | bUC_INT_BUSY | bUC_DMA_EN; // 启动USB主机及DMA,在中断标志未清除前自动暂停
 
 	UH_SETUP = bUH_SOF_EN;
-	USB_INT_FG = 0xFF; // ���жϱ�־
+	USB_INT_FG = 0xFF; // 清中断标志
 	for (i = 0; i < 2; i++)
 	{
-		DisableRootHubPort(i); // ���
+		DisableRootHubPort(i); // 清空
 	}
 	USB_INT_EN = bUIE_TRANSFER | bUIE_DETECT;
 }
@@ -236,14 +236,14 @@ UINT8 USBHostTransact(UINT8 endp_pid, UINT8 tog, UINT16 timeout)
 		// wait for next start of frame
 		while (!(USB_MIS_ST & bUMS_SOF_ACT));
 
-		UH_EP_PID = endp_pid; // ָ������PID��Ŀ�Ķ˵��
-		UIF_TRANSFER = 0;	  // ��������
+		UH_EP_PID = endp_pid; // 指定令牌PID和目的端点号
+		UIF_TRANSFER = 0;	  // 允许传输
 		for (i = WAIT_USB_TOUT_200US; i != 0 && UIF_TRANSFER == 0; i--)
 		{
 			;
 		}
 
-		UH_EP_PID = 0x00; // ֹͣUSB����
+		UH_EP_PID = 0x00; // 停止USB传输
 
 		if (UIF_TRANSFER == 0)
 		{
@@ -252,7 +252,7 @@ UINT8 USBHostTransact(UINT8 endp_pid, UINT8 tog, UINT16 timeout)
 			return (ERR_USB_UNKNOWN);
 		}
 
-		if (UIF_TRANSFER) // �������
+		if (UIF_TRANSFER) // 传输完成
 		{
 			if (U_TOG_OK)
 			{
@@ -274,7 +274,7 @@ UINT8 USBHostTransact(UINT8 endp_pid, UINT8 tog, UINT16 timeout)
 			TRACE1("UHUB1_CTRL=%02X\n", (UINT16)UHUB1_CTRL);
 #endif
 
-			r = USB_INT_ST & MASK_UIS_H_RES; // USB�豸Ӧ��״̬
+			r = USB_INT_ST & MASK_UIS_H_RES; // USB设备应答状态
 			//TRACE1("r:0x%02X\r\n", (UINT16)r);
 
 			if (r == USB_PID_STALL)
@@ -312,38 +312,38 @@ UINT8 USBHostTransact(UINT8 endp_pid, UINT8 tog, UINT16 timeout)
 					{
 						TRACE1("quit at line %d\r\n", (UINT16)__LINE__);
 
-						return (r | ERR_USB_TRANSFER); // ���ǳ�ʱ/����,����Ӧ��
+						return (r | ERR_USB_TRANSFER); // 不是超时/出错,意外应答
 					}
-					break; // ��ʱ����
+					break; // 超时重试
 
 				case USB_PID_IN:
-					if (r == USB_PID_DATA0 && r == USB_PID_DATA1) // ��ͬ�����趪��������
+					if (r == USB_PID_DATA0 && r == USB_PID_DATA1) // 不同步则需丢弃后重试
 					{
-					} // ��ͬ������
+					} // 不同步重试
 					else if (r)
 					{
 						TRACE1("quit at line %d\r\n", (UINT16)__LINE__);
 
-						return (r | ERR_USB_TRANSFER); // ���ǳ�ʱ/����,����Ӧ��
+						return (r | ERR_USB_TRANSFER); // 不是超时/出错,意外应答
 					}
-					break; // ��ʱ����
+					break; // 超时重试
 				default:
 
-					return (ERR_USB_UNKNOWN); // �����ܵ����
+					return (ERR_USB_UNKNOWN); // 不可能的情况
 					break;
 				}
 			}
 		}
-		else // �����ж�,��Ӧ�÷��������
+		else // 其它中断,不应该发生的情况
 		{
-			USB_INT_FG = 0xFF; //���жϱ�־
+			USB_INT_FG = 0xFF; //清中断标志
 		}
 		mDelayuS(25);
 	} while (++TransRetry < 250);
 
 	TRACE1("quit at line %d\r\n", (UINT16)__LINE__);
 
-	return (ERR_USB_TRANSFER); // Ӧ��ʱ
+	return (ERR_USB_TRANSFER); // 应答超时
 }
 
 UINT8 HostCtrlTransfer(USB_SETUP_REQ *pSetupReq, UINT8 MaxPacketSize0, PUINT8 DataBuf, PUINT16 RetLen)
@@ -358,7 +358,7 @@ UINT8 HostCtrlTransfer(USB_SETUP_REQ *pSetupReq, UINT8 MaxPacketSize0, PUINT8 Da
 	mDelayuS(200);
 	if (pLen)
 	{
-		*pLen = 0; // ʵ�ʳɹ��շ����ܳ���
+		*pLen = 0; // 实际成功收发的总长度
 	}
 
 	for (TxCnt = 0; TxCnt < sizeof(USB_SETUP_REQ); TxCnt++)
@@ -368,24 +368,24 @@ UINT8 HostCtrlTransfer(USB_SETUP_REQ *pSetupReq, UINT8 MaxPacketSize0, PUINT8 Da
 
 	UH_TX_LEN = sizeof(USB_SETUP_REQ);
 
-	s = USBHostTransact(USB_PID_SETUP << 4, 0x00, 10000); // SETUP�׶�,200mS��ʱ
+	s = USBHostTransact(USB_PID_SETUP << 4, 0x00, 10000); // SETUP阶段,200mS超时
 	if (s != ERR_SUCCESS)
 	{
 		TRACE1("quit at line %d\r\n", (UINT16)__LINE__);
 
 		return (s);
 	}
-	UH_RX_CTRL = UH_TX_CTRL = bUH_R_TOG | bUH_R_AUTO_TOG | bUH_T_TOG | bUH_T_AUTO_TOG; // Ĭ��DATA1
-	UH_TX_LEN = 0x01;																   // Ĭ�������ݹ�״̬�׶�ΪIN
+	UH_RX_CTRL = UH_TX_CTRL = bUH_R_TOG | bUH_R_AUTO_TOG | bUH_T_TOG | bUH_T_AUTO_TOG; // 默认DATA1
+	UH_TX_LEN = 0x01;																   // 默认无数据故状态阶段为IN
 	RemLen = (pSetupReq->wLengthH << 8) | (pSetupReq->wLengthL);
-	if (RemLen && pBuf) // ��Ҫ�շ�����
+	if (RemLen && pBuf) // 需要收发数据
 	{
-		if (pSetupReq->bRequestType & USB_REQ_TYP_IN) // ��
+		if (pSetupReq->bRequestType & USB_REQ_TYP_IN) // 收
 		{
 			while (RemLen)
 			{
 				mDelayuS(200);
-				s = USBHostTransact(USB_PID_IN << 4, UH_RX_CTRL, 10000); // IN����
+				s = USBHostTransact(USB_PID_IN << 4, UH_RX_CTRL, 10000); // IN数据
 				if (s != ERR_SUCCESS)
 				{
 					TRACE1("quit at line %d\r\n", (UINT16)__LINE__);
@@ -396,7 +396,7 @@ UINT8 HostCtrlTransfer(USB_SETUP_REQ *pSetupReq, UINT8 MaxPacketSize0, PUINT8 Da
 				RemLen -= RxLen;
 				if (pLen)
 				{
-					*pLen += RxLen; // ʵ�ʳɹ��շ����ܳ���
+					*pLen += RxLen; // 实际成功收发的总长度
 				}
 
 				for (RxCnt = 0; RxCnt != RxLen; RxCnt++)
@@ -406,12 +406,12 @@ UINT8 HostCtrlTransfer(USB_SETUP_REQ *pSetupReq, UINT8 MaxPacketSize0, PUINT8 Da
 				}
 				if (USB_RX_LEN == 0 || (USB_RX_LEN < MaxPacketSize0))
 				{
-					break; // �̰�
+					break; // 短包
 				}
 			}
-			UH_TX_LEN = 0x00; // ״̬�׶�ΪOUT
+			UH_TX_LEN = 0x00; // 状态阶段为OUT
 		}
-		else // ��
+		else // 发
 		{
 			while (RemLen)
 			{
@@ -423,7 +423,7 @@ UINT8 HostCtrlTransfer(USB_SETUP_REQ *pSetupReq, UINT8 MaxPacketSize0, PUINT8 Da
 					TxBuffer[TxCnt] = *pBuf;
 					pBuf++;
 				}
-				s = USBHostTransact(USB_PID_OUT << 4 | 0x00, UH_TX_CTRL, 200000 / 20); // OUT����
+				s = USBHostTransact(USB_PID_OUT << 4 | 0x00, UH_TX_CTRL, 200000 / 20); // OUT数据
 				if (s != ERR_SUCCESS)
 				{
 					TRACE1("quit at line %d\r\n", (UINT16)__LINE__);
@@ -433,16 +433,16 @@ UINT8 HostCtrlTransfer(USB_SETUP_REQ *pSetupReq, UINT8 MaxPacketSize0, PUINT8 Da
 				RemLen -= UH_TX_LEN;
 				if (pLen)
 				{
-					*pLen += UH_TX_LEN; // ʵ�ʳɹ��շ����ܳ���
+					*pLen += UH_TX_LEN; // 实际成功收发的总长度
 				}
 			}
 
-			// ״̬�׶�ΪIN
+			// 状态阶段为IN
 		}
 	}
 
 	mDelayuS(200);
-	s = USBHostTransact((UH_TX_LEN ? USB_PID_IN << 4 | 0x00 : USB_PID_OUT << 4 | 0x00), bUH_R_TOG | bUH_T_TOG, 200000 / 20); // STATUS�׶�
+	s = USBHostTransact((UH_TX_LEN ? USB_PID_IN << 4 | 0x00 : USB_PID_OUT << 4 | 0x00), bUH_R_TOG | bUH_T_TOG, 200000 / 20); // STATUS阶段
 	if (s != ERR_SUCCESS)
 	{
 		TRACE1("quit at line %d\r\n", (UINT16)__LINE__);
@@ -451,13 +451,13 @@ UINT8 HostCtrlTransfer(USB_SETUP_REQ *pSetupReq, UINT8 MaxPacketSize0, PUINT8 Da
 	}
 	if (UH_TX_LEN == 0)
 	{
-		return (ERR_SUCCESS); // ״̬OUT
+		return (ERR_SUCCESS); // 状态OUT
 	}
 	if (USB_RX_LEN == 0)
 	{
-		return (ERR_SUCCESS); // ״̬IN,���IN״̬�������ݳ���
+		return (ERR_SUCCESS); // 状态IN,检查IN状态返回数据长度
 	}
-	return (ERR_USB_BUF_OVER); // IN״̬�׶δ���
+	return (ERR_USB_BUF_OVER); // IN状态阶段错误
 }
 
 UINT8 TransferReceive(ENDPOINT *pEndPoint, UINT8 *pData, UINT16 *pRetLen, UINT16 timeout)
